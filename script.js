46731142f9a6077a271611ba3a8aa24c41737dea
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-09-14 09:34:23 +0200

    move object constructor to create book function

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const collection = document.querySelector('#collection'),
       title = document.querySelector('#title'),
       author = document.querySelector('#author'),
       addBtn = document.querySelector('#add-btn');
-      removeBtn = '<input type="submit" value="Remove" id="remove">';
+      
 
 let library = [];
 
-// book details
-function Book(bookTitle, bookAuthor) {
-  this.bookTitle = bookTitle;
-  this.bookAuthor = bookAuthor;
-}
-
-let book = new Book()
-book.bookTitle = title.value;
-book.bookAuthor = author.value;
-
 // create book
 function createBook() {
+  // book details
+  function Book(bookTitle, bookAuthor) {
+    this.bookTitle = bookTitle;
+    this.bookAuthor = bookAuthor;
+  }
+  
+  let book = new Book()
+  book.bookTitle = title.value;
+  book.bookAuthor = author.value;
+
   // create li
   const li = document.createElement('li');
   li.className = "item";
 
-  li.innerHTML =`&nbsp;"${book.bookTitle}"&nbsp;by&nbsp;&nbsp;${book.bookAuthor}&nbsp;&nbsp;${removeBtn}<br><br><hr>`;
+  li.innerHTML =`&nbsp;"${book.bookTitle}"&nbsp;by&nbsp;&nbsp;${book.bookAuthor}&nbsp;&nbsp;<input type="submit" value="Remove" id="remove"><br><br><hr>`;
 
   // add to ul
   collection.appendChild(li);
@@ -63,3 +63,4 @@ function allEventListeners() {
 }
 
 allEventListeners();
+
