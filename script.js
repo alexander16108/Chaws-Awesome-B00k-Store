bfd4b71ae27753c61fc5b0ecf2d22056add0424b
alexander16108 <alexandermayowa05@gmail.com>
2021-09-15 14:23:40 +0100

    Fixing Linter errors

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
 class BookStore {
-  constructor() {
-    this.books = [];
-  }
+	constructor() {
+		this.books = [];
+	}
 
-  addBook() {
-    const arr = [];
-    this.book = arr;
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    // create a new book class to hold multiple values
-    const book = {
-      title,
-      author,
-    };
+	addBook() {
+		const arr = [];
+		this.book = arr;
+		const title = document.getElementById('title').value;
+		const author = document.getElementById('author').value;
+		// create a new book class to hold multiple values
+		const book = {
+			title,
+			author,
+		};
 
-    // add books to the array
-    arr.push(book);
+		// add books to the array
+		arr.push(book);
 
-    // get the current books from local storage
-    const currentBooks = JSON.parse(localStorage.getItem('books'));
+		// get the current books from local storage
+		const currentBooks = JSON.parse(localStorage.getItem('books'));
 
-    // add the new book to the array
-    currentBooks.push(book);
+		// add the new book to the array
+		currentBooks.push(book);
 
-    // add the new books to local storage
-    localStorage.setItem('books', JSON.stringify(currentBooks));
+		// add the new books to local storage
+		localStorage.setItem('books', JSON.stringify(currentBooks));
 
-    // clear the form
-    title.value = '';
-    author.value = '';
-  }
+		// clear the form
+		title.value = '';
+		author.value = '';
+	}
 
-  removeBook(index) {
-    const arr = [];
-    this.book = arr;
-    arr.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(arr));
-  }
+	removeBook(index) {
+		const arr = [];
+		this.book = arr;
+		arr.splice(index, 1);
+		localStorage.setItem('books', JSON.stringify(arr));
+	}
 
-  displayBooks() {
-    let arr = [];
-    const bookStore = new BookStore();
-    this.book = arr;
-    // check if there are any books in local storage
-    if (localStorage.getItem('books') === null) {
-      // if there are no books in local storage create an empty array
-      localStorage.setItem('books', JSON.stringify([]));
-    } else {
-      arr = JSON.parse(localStorage.getItem('books'));
-    }
-    const booksList = document.getElementById('collection');
+	displayBooks() {
+		let arr = [];
+		const bookStore = new BookStore();
+		this.book = arr;
+		// check if there are any books in local storage
+		if (localStorage.getItem('books') === null) {
+			// if there are no books in local storage create an empty array
+			localStorage.setItem('books', JSON.stringify([]));
+		} else {
+			arr = JSON.parse(localStorage.getItem('books'));
+		}
+		const booksList = document.getElementById('collection');
 
-    booksList.innerHTML = '';
+		booksList.innerHTML = '';
 
-    arr.forEach((book) => {
-      const bookElement = document.createElement('li');
-      bookElement.classList.add('item');
-      bookElement.innerHTML = `"${book.title}" by ${book.author}`;
+		arr.forEach((book) => {
+			const bookElement = document.createElement('li');
+			bookElement.classList.add('item');
+			bookElement.innerHTML = `"${book.title}" by ${book.author}`;
 			const line = document.createElement('hr');
-      const removeButton = document.createElement('button');
-      removeButton.classList.add('remove');
-      removeButton.innerHTML = 'Remove';
-      removeButton.addEventListener('click', () => bookStore.removeBook(
-        bookElement.remove(),
-      ));
-      bookElement.append(removeButton, line);
-      booksList.appendChild(bookElement);
-    });
-  }
+			const removeButton = document.createElement('button');
+			removeButton.classList.add('remove');
+			removeButton.innerHTML = 'Remove';
+			removeButton.addEventListener('click', () => bookStore.removeBook(
+				bookElement.remove(),
+			));
+			bookElement.append(removeButton, line);
+			booksList.appendChild(bookElement);
+		});
+	}
 }
 
 const bookStore = new BookStore();
