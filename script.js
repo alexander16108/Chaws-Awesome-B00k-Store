ea34b9eeb91724230de55e571aee6a499bf69c61
alexander16108 <alexandermayowa05@gmail.com>
2021-09-15 17:20:28 +0100

    Fixing Linter Errors In Js file

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 class BookStore {
-  constructor() {
-    this.books = [];
-  }
+	constructor() {
+		this.books = [];
+	}
 
-  addBook(e) {
-    const arr = [];
-    this.book = arr;
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    // create a new book class to hold multiple values
-    const book = {
-      title,
-      author,
-    };
+	addBook(e) {
+		const arr = [];
+		this.book = arr;
+		const title = document.getElementById('title').value;
+		const author = document.getElementById('author').value;
+		// create a new book class to hold multiple values
+		const book = {
+			title,
+			author,
+		};
 
 		const bookList = document.querySelector('#bookList');
 		const message = document.createElement('div');
@@ -22,45 +22,45 @@ class BookStore {
 		message.style.textAlign = 'center';
 		bookList.appendChild(message);
 
-    if (title === '' || author === '') {
-      message.className = 'alert';
-      message.textContent = 'Add a book';
-      message.style.background = 'white';
-      message.style.color = 'red';
+		if (title === '' || author === '') {
+			message.className = 'alert';
+			message.textContent = 'Add a book';
+			message.style.background = 'white';
+			message.style.color = 'red';
 
-      bookList.appendChild(message);
-      setTimeout(() => {
-        document.querySelector('.alert').remove();
-      }, 3000);
-    } else {
-      // add books to the array
-      arr.push(book);
-      // get the current books from local storage
-      const currentBooks = JSON.parse(localStorage.getItem('books'));
+			bookList.appendChild(message);
+			setTimeout(() => {
+				document.querySelector('.alert').remove();
+			}, 3000);
+		} else {
+			// add books to the array
+			arr.push(book);
+			// get the current books from local storage
+			const currentBooks = JSON.parse(localStorage.getItem('books'));
 
-      // add the new book to the array
-      currentBooks.push(book);
+			// add the new book to the array
+			currentBooks.push(book);
 
-      // add the new books to local storage
-      localStorage.setItem('books', JSON.stringify(currentBooks));
+			// add the new books to local storage
+			localStorage.setItem('books', JSON.stringify(currentBooks));
 
-      message.className = 'alert';
-      message.textContent = `You've added a book!`;
-      message.style.color = 'green';
-      message.style.background = 'white';
-      bookList.appendChild(message);
-      // setTimeout(() => {
-      //   document.querySelector('.alert').remove();
-      // }, 3000);
+			message.className = 'alert';
+			message.textContent = `You've added a book!`;
+			message.style.color = 'green';
+			message.style.background = 'white';
+			bookList.appendChild(message);
+			// setTimeout(() => {
+			//   document.querySelector('.alert').remove();
+			// }, 3000);
 
-      // clear the form
-      title.value = '';
-      author.value = '';
-    }
-    e.preventDefault();
-  }
+			// clear the form
+			title.value = '';
+			author.value = '';
+		}
+		e.preventDefault();
+	}
 
-  removeBook(index) {
+	removeBook(index) {
 		const bookList = document.querySelector('#bookList');
 		const message = document.createElement('div');
 		message.className = 'alert';
@@ -68,45 +68,45 @@ class BookStore {
 		message.style.color = 'red';
 		message.style.background = 'white';
 		bookList.appendChild(message);
-		      setTimeout(() => {
-        document.querySelector('.alert').remove();
-      }, 3000);
-    const arr = [];
-    this.book = arr;
-    arr.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(arr));
-  }
+		setTimeout(() => {
+			document.querySelector('.alert').remove();
+		}, 3000);
+		const arr = [];
+		this.book = arr;
+		arr.splice(index, 1);
+		localStorage.setItem('books', JSON.stringify(arr));
+	}
 
-  displayBooks() {
-    let arr = [];
-    const bookStore = new BookStore();
-    this.book = arr;
-    // check if there are any books in local storage
-    if (localStorage.getItem('books') === null) {
-      // if there are no books in local storage create an empty array
-      localStorage.setItem('books', JSON.stringify([]));
-    } else {
-      arr = JSON.parse(localStorage.getItem('books'));
-    }
-    const booksList = document.getElementById('collection');
+	displayBooks() {
+		let arr = [];
+		const bookStore = new BookStore();
+		this.book = arr;
+		// check if there are any books in local storage
+		if (localStorage.getItem('books') === null) {
+			// if there are no books in local storage create an empty array
+			localStorage.setItem('books', JSON.stringify([]));
+		} else {
+			arr = JSON.parse(localStorage.getItem('books'));
+		}
+		const booksList = document.getElementById('collection');
 
-    booksList.innerHTML = '';
+		booksList.innerHTML = '';
 
-    arr.forEach((book) => {
-      const bookElement = document.createElement('li');
-      bookElement.classList.add('item');
-      bookElement.innerHTML = `"${book.title}" by ${book.author}`;
-      const removeButton = document.createElement('button');
-      removeButton.classList.add('remove');
-      removeButton.innerHTML = 'Remove';
-      removeButton.addEventListener('click', () => bookStore.removeBook(
-        bookElement.remove(),
-      ));
-      bookElement.append(removeButton);
-      booksList.appendChild(bookElement);
-    });
+		arr.forEach((book) => {
+			const bookElement = document.createElement('li');
+			bookElement.classList.add('item');
+			bookElement.innerHTML = `"${book.title}" by ${book.author}`;
+			const removeButton = document.createElement('button');
+			removeButton.classList.add('remove');
+			removeButton.innerHTML = 'Remove';
+			removeButton.addEventListener('click', () => bookStore.removeBook(
+				bookElement.remove(),
+			));
+			bookElement.append(removeButton);
+			booksList.appendChild(bookElement);
+		});
 
-  }
+	}
 }
 
 const bookStore = new BookStore();
