a449a9aeefb403ff34caf2b1ac46c8623b0e4b2f
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-09-16 10:37:35 +0200

    Add event listeners so the sections pop up when a button is clicked

---
+++ js/nav.js
@@ -0,0 +1,36 @@
+const list = document.querySelector('.list'),
+      add = document.querySelector('.add'),
+      contact = document.querySelector('.navContact')
+      listSec = document.querySelector('#bookList'),
+      addSec = document.querySelector('#add_book'),
+      contactSec = document.querySelector('#contact'),
+      date = document.querySelector('.date');
+
+// display list section
+function displayList() {
+  listSec.style.display = 'block';
+  addSec.style.display = 'none';
+  contactSec.style.display = 'none';
+}
+
+// display add section
+function displayAdd() {
+  listSec.style.display = 'none';
+  addSec.style.display = 'block';
+  contactSec.style.display = 'none';
+}
+
+// display add section
+function displayContact() {
+  listSec.style.display = 'none';
+  addSec.style.display = 'none';
+  contactSec.style.display = 'block';
+}
+
+function allEventListenters() {
+  add.addEventListener('click', displayAdd);
+  list.addEventListener('click', displayList);
+  contact.addEventListener('click', displayContact);
+}
+
+allEventListenters();
